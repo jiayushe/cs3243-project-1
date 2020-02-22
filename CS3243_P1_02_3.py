0c106e156323f7e7ca284e772eb6793b569ad7f1
SebastianLie <jackcass629@gmail.com>
2020-02-22 14:30:27 +0800

    Improved runtime for Manhattan Distance

--- CS3243_P1_02_3.py
+++ CS3243_P1_02_3.py
@@ -1,8 +1,7 @@
 import os
 import sys
 from heapq import heappush, heappop, heapify
-import copy
-import math
+import time
 
 ## GLOBAL CONSTANTS ##
 
@@ -59,33 +58,17 @@ class Node:
         self.tree_depth = depth
         self.blank = blank_pos
 
-    def __cmp__(self, obj):
-        if not isinstance(obj, Node):
-            raise Exception("Node compared to a non-node object!")
-        if self.fval > obj.fval:
-            return 1
-        elif self.fval < obj.fval:
-            return -1
-        else:
-            if self.tree_depth < obj.tree_depth:
-                return -1
-            elif self.tree_depth > obj.tree_depth:
-                return 1
-            else:
-                return 0
-    def __ne__(self, obj):
-        if obj == None:
-            return True
-        if not isinstance(obj, Node):
-            raise Exception("Node compared to a non-node object!")
-        return self.state != obj.state
-
 class Puzzle(object):
     def __init__(self, init_state, goal_state):
         self.grid_size = len(init_state)
         self.init_state = self.list_to_tuple(init_state)
         self.goal_state = self.list_to_tuple(goal_state)
         self.goal = None
+        self.pq = PriorityQueue()
+        self.seen_states = set()
+        self.goal_position = {} # a map from number to its goal position
+        for i in range(len(self.goal_state)):
+            self.goal_position[self.goal_state[i]] = i
 
     ## Helper Functions ##
 
@@ -106,6 +89,10 @@ class Puzzle(object):
         for i in range(self.grid_size**2):
             if self.init_state[i] == 0:
                 return i
+    
+    
+    def hash(self, state):
+        return "".join(map(str,state))
 
     #########################
     
@@ -114,7 +101,7 @@ class Puzzle(object):
         Idea adapted from https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
     
     '''
-    def solvable(self, state, blank):
+    def is_solvable(self, state, blank):
         inversions = self.count_inversions(state)
         if self.grid_size % 2 == 1:  # grid size odd
             if inversions % 2 == 1:
@@ -144,67 +131,40 @@ class Puzzle(object):
                     inversions += 1
         return inversions
         
-    # todo optimise to pass n=4 case 2 and 3 in < 5s
     def solve(self):
-        
+        k = self.grid_size
+        upper_bound = k**4
         blank_position = self.find_blank()
-        if not self.solvable(self.init_state, blank_position):
+        if not self.is_solvable(self.init_state, blank_position):
             return ["UNSOLVABLE"]
-        
+        heap = []
         moves = [left,right,down,up]
-        pq = PriorityQueue()
-        seen_states = set()
-        seen_states.add(self.init_state)
         curr_f_val = self.heuristic(self.init_state)
-        curr_node = Node(curr_f_val, self.init_state, None, None, 0, blank_position)
         
-        while True:        
-            for move in moves:
-                if self.is_valid_move(move, curr_node.blank):
-                    new_node = self.make_move(move, curr_node)
-                
-                    if new_node.state not in seen_states:
-                        pq.add(new_node)
-                        seen_states.add(new_node.state)
-                        
-            # choose state with lowest f value
-            curr_node = pq.poll()  
-            if curr_node.fval - curr_node.tree_depth == 0:
-                # goal reached, since heuristic is 0
+        root = Node(curr_f_val, self.init_state, None, None, 0, blank_position)
+        heappush(heap,(curr_f_val,root))
+        while True:
+            curr_node = heappop(heap)[1]
+            while curr_node.tree_depth > upper_bound:
+                curr_node = heappop(heap)[1]
+            self.seen_states.add(hash(curr_node.state))
+            if curr_node.state == self.goal_state:
                 self.goal = curr_node
                 break
-            
+            for move in moves:
+                new_node = self.make_move(move,curr_node)
+                if new_node is not None:
+                    if hash(new_node.state) not in self.seen_states:
+                        heappush(heap,(new_node.fval, new_node))
+                        self.seen_states.add(hash(new_node.state))
+                        
         solution = []
+        
         # travel from goal node to initial, recording moves.
         while curr_node.parent != None:
             solution.insert(0,curr_node.move)
             curr_node = curr_node.parent  
         return solution
-
-    #todo merge validity with making move so no check needed 
-    def is_valid_move(self, move, blank_position): # ensure move generates new state
-        
-        k = self.grid_size
-        blank_column = blank_position % k 
-        blank_row = blank_position // k
-        
-        if move == left:
-            if blank_column == k-1:
-                # blank on rightmost column
-                return False
-        elif move == right:
-            if blank_column == 0:
-                # blank on leftmost column
-                return False 
-        elif move == up:
-            if blank_row == k-1:
-                # blank on top row
-                return False  
-        else:
-            if blank_row == 0:
-                # blank on bottom row
-                return False  
-        return True
 
     def make_move(self, move, curr_node):
         blank_position = curr_node.blank
@@ -215,32 +175,40 @@ class Puzzle(object):
         g_n = curr_node.tree_depth + 1
         
         if move == left:
-            # blank switches place with number to it's right 
-            
-            new_state[blank_position + 1], new_state[blank_position] = new_state[blank_position], new_state[blank_position + 1]
-            func_value = self.heuristic(new_state) + g_n
-            new_blank_position = blank_position+1
+            # blank switches place with number to it's right, can't be last col
+            if blank_column < k-1:
+                new_state[blank_position + 1], new_state[blank_position] = new_state[blank_position], new_state[blank_position + 1]
+                func_value = self.heuristic(new_state) + g_n
+                new_blank_position = blank_position+1
+            else:
+                return None
 
         elif move == right:
-            # blank switches place with number to it's left 
-            
-            new_state[blank_position - 1], new_state[blank_position] = new_state[blank_position], new_state[blank_position - 1]
-            func_value = self.heuristic(new_state) + g_n
-            new_blank_position = blank_position-1
+            # blank switches place with number to it's left, can't be first col
+            if blank_column > 0:
+                new_state[blank_position - 1], new_state[blank_position] = new_state[blank_position], new_state[blank_position - 1]
+                func_value = self.heuristic(new_state) + g_n
+                new_blank_position = blank_position-1
+            else:
+                return None
 
         elif move == down:
-            # blank switches place with number above it
-            
-            new_state[(blank_row-1) * k + blank_column], new_state[blank_position] = new_state[blank_position], new_state[(blank_row-1) * k + blank_column]
-            func_value = self.heuristic(new_state) + g_n
-            new_blank_position = (blank_row-1) * k + blank_column
+            # blank switches place with number above it, can't be first row
+            if blank_row > 0:
+                new_state[(blank_row-1) * k + blank_column], new_state[blank_position] = new_state[blank_position], new_state[(blank_row-1) * k + blank_column]
+                func_value = self.heuristic(new_state) + g_n
+                new_blank_position = (blank_row-1) * k + blank_column
+            else:
+                return None
 
         else:
-            # blank switches place with number below it
-            
-            new_state[(blank_row+1) * k + blank_column], new_state[blank_position] = new_state[blank_position], new_state[(blank_row+1) * k + blank_column]
-            func_value = self.heuristic(new_state) + g_n
-            new_blank_position = (blank_row+1) * k + blank_column
+            # blank switches place with number below it, can't be last row
+            if blank_row < k-1:
+                new_state[(blank_row+1) * k + blank_column], new_state[blank_position] = new_state[blank_position], new_state[(blank_row+1) * k + blank_column]
+                func_value = self.heuristic(new_state) + g_n
+                new_blank_position = (blank_row+1) * k + blank_column
+            else:
+                return None
 
         new_node = Node(func_value, tuple(new_state), curr_node, move, g_n, new_blank_position)
         return new_node
@@ -267,7 +235,6 @@ class Puzzle(object):
                 if curr_tile == 0:
                     continue
                 if curr_tile%k == 0:
-                    
                     # account for list indexing, usually should be one row above
                     correct_row = curr_tile//k - 1
                     # since k-multiple always on last col
@@ -281,6 +248,22 @@ class Puzzle(object):
                 total_tile_distance += num_tiles_away
                 
         return total_tile_distance
+
+    def linear_conflict(self, state):
+        count = 0
+        for row in range(self.grid_size):
+            for k in range(self.N):
+                if state[row*self.N + k] == 0:
+                    continue
+                for j in range(k+1, self.grid_size):
+                    if state[row*self.N + j] == 0:
+                        continue
+                    # now t_j is guaranteed to be on the same line, right of t_k
+                    goal_pos_j = self.goal_position[state[row*self.N + j]]
+                    goal_pos_k = self.goal_position[state[row*self.N + k]]
+                    if (goal_pos_j // self.N == goal_pos_k // self.N) and (goal_pos_j % self.N < goal_pos_k % self.N):
+                        count += 1
+        return count * 2 + self.manhattan_distance(state)
 
     ## Verifying correctness of solution ##
     
@@ -327,22 +310,22 @@ class Puzzle(object):
 
             self.pretty_print(curr_state)
 
-'''
 #n = 3 cases
 #init_state = [[2,3,6],[1,5,8],[4,7,0]]
 #init_state = [[1,2,3],[4,5,6],[8,7,0]] # unsolvable 
 #init_state = [[1,8,3],[5,2,4],[0,7,6]]
 #init_state = [[1,2,3],[4,5,6],[7,0,8]]
 #init_state = [[8,6,7],[2,5,4],[3,0,1]]
-
 #goal_state = [[1,2,3],[4,5,6],[7,8,0]]
 
 # n = 4 cases
 
 #init_state = [[1,2,3,4],[5,6,7,8],[10,11,0,12],[9,13,15,14]]  # unsolvable
 #init_state = [[12,15,6,10],[4,9,5,8],[14,13,0,2],[1,7,11,3]]
-#init_state = [[14,10,5,13],[11,8,1,3],[2,9,12,6],[15,4,0,7]]
-#goal_state = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
+
+#init_state = [[13,5,3,4],[2,1,8,0],[9,15,10,11],[14,12,6,7]]
+init_state = [[9,5,12,4],[0,1,3,10],[14,13,11,2],[15,7,6,8]]
+goal_state = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
 
 
 # n = 5 cases
@@ -352,16 +335,20 @@ class Puzzle(object):
 
 #init_state = [[1,3,4,0,10],[7,2,12,8,5],[6,11,13,15,14],[17,23,18,9,19],[16,21,22,24,20]]
 #init_state = [[1,3,4,10,5],[7,2,12,8,14],[6,11,13,15,0],[17,23,18,9,19],[16,21,22,24,20]]
-init_state = [[1,2,3,4,5],[6,7,8,9,10],[11,12,14,0,15],[16,17,13,18,19],[21,22,23,24,20]]
+#init_state = [[1,2,3,4,5],[6,7,8,9,10],[11,12,14,0,15],[16,17,13,18,19],[21,22,23,24,20]]
 #init_state = [[1,2,3,4,5],[6,7,8,9,10],[11,12,0,14,15],[16,17,13,18,19],[21,22,23,20,24]]   # unsolvable 
 
-goal_state = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,0]]
+#goal_state = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,0]]
 
+
+t0 = time.time()
 
 puzzle = Puzzle(init_state, goal_state)
 ans = puzzle.solve()
+t1 = time.time()
 puzzle.verify_solution(ans)
 print ans
+print t1-t0
 
 '''
 if __name__ == "__main__":
@@ -413,3 +400,5 @@ if __name__ == "__main__":
     with open(sys.argv[2], 'a') as f:
         for answer in ans:
             f.write(answer+'\n')
+
+'''
