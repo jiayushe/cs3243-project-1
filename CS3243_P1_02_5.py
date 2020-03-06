2516900ef4a1e3a8627602adf82bb65c558e97c5
Tan Yuanhong <leotan.yh@gmail.com>
2020-03-06 17:30:03 +0800

    Automate experiment

--- CS3243_P1_02_5.py
+++ CS3243_P1_02_5.py
@@ -3,7 +3,7 @@ import sys
 import random
 import time
 import shlex
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from threading import Timer
 
 def echo_cyan(string):
@@ -106,7 +106,7 @@ def generate_input(dimension, sample_size, prefix):
 def mean(lst):
     if len(lst) == 0:
         return 0
-    return sum(lst) / len(lst)
+    return float(sum(lst)) / len(lst)
 
 def stdev(lst):
     if len(lst) == 0:
@@ -115,7 +115,7 @@ def stdev(lst):
     res = 0
     for i in lst:
         res += (i - m) ** 2
-    return (res / len(lst)) ** 0.5
+    return (float(res) / len(lst)) ** 0.5
 
 def maximum(lst):
     if len(lst) == 0:
@@ -127,20 +127,27 @@ if __name__ == '__main__':
     random.seed(1)
     time_limit = 60
     sample_size = 10
+    result = []
     for dimension in [3, 4, 5]:
         echo_cyan('Dimension = ' + str(dimension))
         prefix = 'experiment/test_' + str(dimension) + '_'
         generate_input(dimension, sample_size, prefix)
         for solution_index in [1, 2, 3, 4]:
+            if solution_index == 1 and dimension != 3:
+                continue # skip n=4,5 for BFS
             solution_file = 'CS3243_P1_02_' + str(solution_index) + '.py'
             echo_cyan('Solution = ' + str(solution_file))
             runtime_arr = []
+            solution_depth_arr = []
+            explored_states_arr = []
+            generated_states_arr = []
+            max_heap_size_arr = []
             for cnt in range(0, sample_size):
                 input_file = prefix + str(cnt) + '.in'
                 output_file = prefix + str(cnt) + '_' + str(solution_index) + '.out'
                 cmd = 'python ' + solution_file + ' ' + input_file + ' ' + output_file
                 echo_cyan(input_file)
-                process = Popen(shlex.split(cmd))
+                process = Popen(shlex.split(cmd), stderr=PIPE, stdout=PIPE)
                 timer = Timer(time_limit, kill, [process])
                 start_time = time.time()
                 timer.start()
@@ -149,14 +156,44 @@ if __name__ == '__main__':
                 runtime = time.time() - start_time
                 if return_code == 0:
                     runtime_arr.append(runtime)
+                    stdout, stderr = process.communicate()
+                    res = stderr.split('\n')
+                    solution_depth_arr.append(int(res[0].split(':')[1]))
+                    explored_states_arr.append(int(res[1].split(':')[1]))
+                    generated_states_arr.append(int(res[2].split(':')[1]))
+                    max_heap_size_arr.append(int(res[3].split(':')[1]))
                     sys.stdout.write('Puzzle solved (' + str(runtime) + 's)\n')
                 else:
                     sys.stdout.write('Time limit exceeded (' + str(time_limit) + 's)\n')
-            echo_cyan('Pass Rate:')
-            sys.stdout.write(str(len(runtime_arr) // sample_size * 100) + '%\n')
-            echo_cyan('Average Runtime:')
+            echo_cyan('Pass rate:')
+            pass_rate = float(len(runtime_arr)) / sample_size * 100
+            sys.stdout.write(str(pass_rate) + '%\n')
+            echo_cyan('Average runtime:')
             sys.stdout.write(str(mean(runtime_arr)) + 's\n')
-            echo_cyan('Standard Deviation:')
+            echo_cyan('Runtime standard deviation:')
             sys.stdout.write(str(stdev(runtime_arr)) + 's\n')
-            echo_cyan('Max Runtime:')
+            echo_cyan('Maximum runtime:')
             sys.stdout.write(str(maximum(runtime_arr)) + 's\n')
+            echo_cyan('Average solution depth:')
+            sys.stdout.write(str(mean(solution_depth_arr)) + '\n')
+            echo_cyan('Average explored states:')
+            sys.stdout.write(str(mean(explored_states_arr)) + '\n')
+            echo_cyan('Average generated states:')
+            sys.stdout.write(str(mean(generated_states_arr)) + '\n')
+            echo_cyan('Average max heap size:')
+            sys.stdout.write(str(mean(max_heap_size_arr)) + '\n')
+            sys.stdout.write('\n')
+            result.append(( "(%d, %d)" % (dimension, solution_index),
+                            pass_rate, 
+                            mean(runtime_arr), 
+                            stdev(runtime_arr), 
+                            maximum(runtime_arr), 
+                            mean(solution_depth_arr), 
+                            mean(explored_states_arr), 
+                            mean(generated_states_arr), 
+                            mean(max_heap_size_arr)))
+    print("test case,pass rate,avg runtime,runtime stdev,max runtime,avg sol depth,avg explore states,avg generated states,avg max heap size")
+    for res in result:
+        for item in res:
+            sys.stdout.write(str(item) + ",")
+        sys.stdout.write("\n")
